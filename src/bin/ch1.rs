7beeccaeede3db6cc516f105502aa04e8a2bd271
maxgmr <mxzmxz120@gmail.com>
2024-05-21 21:29:16 -0700

    Q1.5-7

--- src/bin/ch1.rs
+++ src/bin/ch1.rs
@@ -107,6 +107,115 @@ fn palin_perm(input_string: String) -> bool {
     }
 }
 
+// 1.5 One Away
+// Check if two strings are 1 or 0 edits (add, remove, change char) away.
+fn one_away(sa: &str, sb: &str) -> bool {
+    let len_diff = sa.len() as i32 - sb.len() as i32;
+    if len_diff.unsigned_abs() as usize > 1 {
+        return false;
+    }
+    let ca: Vec<char> = sa.chars().collect();
+    let cb: Vec<char> = sb.chars().collect();
+
+    let mut edited: bool = false;
+    let mut a_shift: usize = 0;
+    let mut b_shift: usize = 0;
+
+    for i in 0..(match len_diff {
+        1 => ca.len(),
+        _ => cb.len(),
+    }) {
+        if ca[i - a_shift] != cb[i - b_shift] {
+            if edited {
+                return false;
+            } else {
+                edited = true;
+                match len_diff {
+                    1 => b_shift = 1,
+                    -1 => a_shift = 1,
+                    _ => (),
+                }
+            }
+        }
+    }
+    true
+}
+
+// 1.6 String Compression
+// Condense repeated characters
+fn string_compression(s: String) -> String {
+    struct CharCount {
+        char: char,
+        count: usize,
+    }
+    impl CharCount {
+        fn as_string(&self) -> String {
+            let mut string = String::from(self.char);
+            string.push_str(&self.count.to_string());
+            string
+        }
+    }
+
+    let mut tracker: Vec<CharCount> = Vec::with_capacity(s.len());
+    let mut i: usize = 0;
+    let cs: Vec<char> = s.chars().collect();
+
+    while i < s.len() {
+        let char = cs[i];
+        let mut count: usize = 1;
+        while ((i + 1) < cs.len()) && (cs[i] == cs[i + 1]) {
+            count += 1;
+            i += 1;
+        }
+        tracker.push(CharCount { char, count });
+        i += 1;
+    }
+    let mut output = String::new();
+    tracker
+        .iter()
+        .for_each(|cc| output.push_str(&cc.as_string()));
+    if output.len() < s.len() {
+        output
+    } else {
+        s
+    }
+}
+
+// 1.7 Rotate Matrix
+// Rotate NxN matrix with pixel = [u8; 4] 90deg CW
+// Try to do it in place!
+fn rotate_matrix(mut mat: Vec<Vec<[u8; 4]>>) -> Vec<Vec<[u8; 4]>> {
+    // For i in 0..(N/2).ceil()
+    //  For j in (0+i)..((N-1)-i)
+    //      "Follow trail"
+    let n = mat.len();
+    mat.iter().for_each(|row| {
+        if row.len() != mat.len() {
+            panic!("Matrix height and width must be equal.");
+        }
+    });
+
+    fn rotated_coords((i, j): (usize, usize), n: usize) -> (usize, usize) {
+        (((n - 1) - j), i)
+    }
+
+    for j in 0_usize..((n as f32 / 2.0_f32).round() as usize) {
+        for i in j..((n - 1) - j) {
+            let (i2, j2) = rotated_coords((i, j), n);
+            let (i3, j3) = rotated_coords((i2, j2), n);
+            let (i4, j4) = rotated_coords((i3, j3), n);
+            let temp_val = mat[j2][i2];
+            mat[j2][i2] = mat[j][i];
+            let temp_val_2 = mat[j3][i3];
+            mat[j3][i3] = temp_val;
+            let temp_val = mat[j4][i4];
+            mat[j4][i4] = temp_val_2;
+            mat[j][i] = temp_val;
+        }
+    }
+    mat
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -168,5 +277,186 @@ mod tests {
     #[test]
     fn pp_no() {
         assert_eq!(palin_perm(String::from("snafu")), false);
+    }
+
+    #[test]
+    fn oa_same() {
+        assert_eq!(one_away("fish", "fish"), true);
+    }
+
+    #[test]
+    fn oa_1_in() {
+        assert_eq!(one_away("ple", "pale"), true);
+    }
+
+    #[test]
+    fn oa_2_in() {
+        assert_eq!(one_away("ple", "paled"), false);
+    }
+
+    #[test]
+    fn oa_1_rm() {
+        assert_eq!(one_away("fish", "fsh"), true);
+    }
+
+    #[test]
+    fn oa_2_rm() {
+        assert_eq!(one_away("fish", "is"), false);
+    }
+
+    #[test]
+    fn oa_1_sw() {
+        assert_eq!(one_away("fish", "fist"), true);
+    }
+
+    #[test]
+    fn oa_2_sw() {
+        assert_eq!(one_away("fish", "list"), false);
+    }
+
+    #[test]
+    fn oa_sp() {
+        assert_eq!(one_away("fish", "fi sh"), true);
+    }
+
+    #[test]
+    fn oa_sw_rm() {
+        assert_eq!(one_away("fish", "lis"), false);
+    }
+
+    #[test]
+    fn oa_sw_in() {
+        assert_eq!(one_away("ist", "fish"), false);
+    }
+
+    #[test]
+    fn sc_same() {
+        assert_eq!(string_compression(String::from("babbbb")), "babbbb");
+    }
+
+    #[test]
+    fn sc_bigger() {
+        assert_eq!(string_compression(String::from("fish")), "fish");
+    }
+
+    #[test]
+    fn sc_compress() {
+        assert_eq!(string_compression(String::from("dooooooom")), "d1o7m1");
+    }
+
+    #[test]
+    fn sc_10_bigger() {
+        assert_eq!(
+            string_compression(String::from("doooooooooomnbvcxzlakjh")),
+            "doooooooooomnbvcxzlakjh"
+        );
+    }
+
+    #[test]
+    fn sc_alternating() {
+        assert_eq!(
+            string_compression(String::from("ooobbbboooooooobbooooo")),
+            "o3b4o8b2o5"
+        );
+    }
+
+    #[test]
+    #[should_panic]
+    fn rm_panic() {
+        rotate_matrix(vec![vec![[1, 2, 3, 4], [255, 255, 255, 255]]]);
+    }
+
+    #[test]
+    #[should_panic]
+    fn rm_panic_2() {
+        rotate_matrix(vec![vec![[0, 0, 0, 0]], vec![[255, 255, 255, 255]]]);
+    }
+
+    #[test]
+    fn rm_1x1() {
+        #[allow(clippy::useless_vec)]
+        let ans = vec![vec![[0, 0, 0, 0]]];
+        let result = rotate_matrix(vec![vec![[0, 0, 0, 0]]]);
+        for i in 0..1 {
+            for j in 0..1 {
+                assert_eq!(ans[i][j], result[i][j]);
+            }
+        }
+    }
+
+    #[test]
+    fn rm_2x2() {
+        #[allow(clippy::useless_vec)]
+        let ans = vec![
+            vec![[3, 3, 3, 3], [1, 1, 1, 1]],
+            vec![[4, 4, 4, 4], [2, 2, 2, 2]],
+        ];
+        #[allow(clippy::useless_vec)]
+        let result = rotate_matrix(vec![
+            vec![[1, 1, 1, 1], [2, 2, 2, 2]],
+            vec![[3, 3, 3, 3], [4, 4, 4, 4]],
+        ]);
+        assert_eq!(result, ans);
+    }
+
+    #[test]
+    fn rm_3x3() {
+        #[allow(clippy::useless_vec)]
+        let ans = vec![
+            vec![[1, 1, 1, 1], [2, 2, 2, 2], [3, 3, 3, 3]],
+            vec![[4, 4, 4, 4], [5, 5, 5, 5], [6, 6, 6, 6]],
+            vec![[7, 7, 7, 7], [8, 8, 8, 8], [9, 9, 9, 9]],
+        ];
+        #[allow(clippy::useless_vec)]
+        let result = rotate_matrix(vec![
+            vec![[3, 3, 3, 3], [6, 6, 6, 6], [9, 9, 9, 9]],
+            vec![[2, 2, 2, 2], [5, 5, 5, 5], [8, 8, 8, 8]],
+            vec![[1, 1, 1, 1], [4, 4, 4, 4], [7, 7, 7, 7]],
+        ]);
+        assert_eq!(result, ans);
+    }
+
+    #[test]
+    fn rm_4x4() {
+        #[allow(clippy::useless_vec)]
+        let ans = vec![
+            vec![[1, 1, 1, 1], [2, 2, 2, 2], [3, 3, 3, 3], [4, 4, 4, 4]],
+            vec![[5, 5, 5, 5], [6, 6, 6, 6], [7, 7, 7, 7], [8, 8, 8, 8]],
+            vec![
+                [9, 9, 9, 9],
+                [10, 10, 10, 10],
+                [11, 11, 11, 11],
+                [12, 12, 12, 12],
+            ],
+            vec![
+                [13, 13, 13, 13],
+                [14, 14, 14, 14],
+                [15, 15, 15, 15],
+                [16, 16, 16, 16],
+            ],
+        ];
+        #[allow(clippy::useless_vec)]
+        let result = rotate_matrix(vec![
+            vec![
+                [4, 4, 4, 4],
+                [8, 8, 8, 8],
+                [12, 12, 12, 12],
+                [16, 16, 16, 16],
+            ],
+            vec![
+                [3, 3, 3, 3],
+                [7, 7, 7, 7],
+                [11, 11, 11, 11],
+                [15, 15, 15, 15],
+            ],
+            vec![
+                [2, 2, 2, 2],
+                [6, 6, 6, 6],
+                [10, 10, 10, 10],
+                [14, 14, 14, 14],
+            ],
+            vec![[1, 1, 1, 1], [5, 5, 5, 5], [9, 9, 9, 9], [13, 13, 13, 13]],
+        ]);
+        assert_eq!(ans, result);
     }
 }
